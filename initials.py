367f23d75a1312b1ec7f2c72e27e502bc4ee3769
Herman Bannerman-Hesse <142167174+1RealHerman@users.noreply.github.com>
2026-05-30 20:49:20 +0000

    1st Assignment
    This program prints the initials 'HBH' in large stylized letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Herman Bannerman-Hesse
+# STUDENT ID: 2425401086
+# Email: hermanbhesse@gmail.com
+# Grader: Augustus
+#
+# On my honor, Herman Bannerman-Hesse, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This program prints my initials in large stylized letters
 
+  print()
+  print("...HBH")
+  print()
 
-
-main()
+  print("...HHH......HHH........BBBBBBBBBBBB........HHH......HHH")
+  print("...HHH......HHH........BBBBBBBBBBBB........HHH......HHH")
+  print("...HHH......HHH........BBB......BBB........HHH......HHH")
+  print("...HHH......HHH........BBB......BBB........HHH......HHH")
+  print("...HHHHHHHHHHHH........BBBBBBBBBBBB........HHHHHHHHHHHH")
+  print("...HHHHHHHHHHHH........BBBBBBBBBBBB........HHHHHHHHHHHH")
+  print("...HHH......HHH........BBB......BBB........HHH......HHH")
+  print("...HHH......HHH........BBB......BBB........HHH......HHH")
+  print("...HHH......HHH...**...BBBBBBBBBBBB...**...HHH......HHH")
+  print("...HHH......HHH...**...BBBBBBBBBBBB...**...HHH......HHH")
+  print()
+ 
+main()  
